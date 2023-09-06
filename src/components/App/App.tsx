55c2a47e6c4d5363bd47e0bd6a127c9c70768123
Svetlana Dyakova <sdyakova@outlook.com>
2023-09-06 15:43:47 +0700

    add task

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { ReactDOM } from 'react';
 import classes from './App.module.scss';
 import NewTaskForm from '../NewTaskForm';
+import Task from '../Task/Task';
 
-function App() {
+const App = () => {
   return (
     <div className={classes.container}>
       <h1 className={classes.header}>todos</h1>
       <NewTaskForm />
+      <Task />
     </div>
   );
-}
+};
 
 export default App;
