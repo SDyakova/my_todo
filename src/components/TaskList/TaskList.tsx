9272ef720edc133c59d1f012c1ebb03aedcdb7d9
Svetlana Dyakova <sdyakova@outlook.com>
2023-09-08 15:41:40 +0700

    initial commit

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import classes from './TaskList.module.scss';
+
+const TaskList = () => {
+  return <div>Task</div>;
+};
+
+export default TaskList;
